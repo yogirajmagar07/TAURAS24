c273493db4dcfde0f5649da63009e3d77972ead9
yogirajmagar07 <99736643+yogirajmagar07@users.noreply.github.com>
2026-06-01 17:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,12 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 # =========================
 TABLES_CONN = os.environ.get("TABLES_CONNECTION_STRING")
 TABLE_NAME = os.environ.get("TABLE_NAME", "DeviceData")
-TABLE_NAME_2 = os.environ.get("TABLE_NAME_2", "ScadaData")
 
 service = TableServiceClient.from_connection_string(TABLES_CONN)
 table_client = service.get_table_client(TABLE_NAME)
-table_client_2 = service.get_table_client(TABLE_NAME_2)
 
 try:
     table_client.create_table()
-    table_client_2.create_table()
 except:
     pass
 
@@ -136,27 +133,6 @@ def ingest():
         entity = build_entity(data)
 
         table_client.upsert_entity(entity=entity, mode="replace")
-
-        # ✅ FIX 2 — Multi Device Cache
-        latest_cache[entity["PartitionKey"]] = entity
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-    return jsonify({"status": "ok"})
-
-@app.route("/load", methods=["POST"])
-def load_data():
-
-    if not request.is_json:
-        return jsonify({"error": "JSON required"}), 400
-
-    data = request.get_json()
-
-    try:
-        entity = build_entity(data)
-
-        table_client_2.upsert_entity(entity=entity, mode="replace")
 
         # ✅ FIX 2 — Multi Device Cache
         latest_cache[entity["PartitionKey"]] = entity
